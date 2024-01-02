ef1bc773092768ebce007780dd9c4f8021aaa2dd
Sumit Agarwal <agawralsumit334ju@gmail.com>
2024-01-02 20:28:00 +0530

    first commit

---
+++ controller/image-controller.js
@@ -0,0 +1,36 @@
+import File from "../models/file.js";
+
+
+export const uploadImage = async (req,res)=>{
+
+    const fileObj={
+        path: req.file.path,
+        name: req.file.originalname
+    }
+
+    try {
+        const file = await File.create(fileObj);
+        res.status(200).json({
+            path: `http://localhost:8000/file/${file._id}`
+        })
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({error: error.message})
+    }
+}
+
+export const downloadImage= async(req,res)=>{
+    try {
+        const file= await File.findById(req.params.fileId);
+        file.downloadContent++;
+
+        await file.save();
+
+        res.download(file.path,file.name);
+    } catch (error) {
+        console.error(error.message);
+        return res.status(500).json({
+            error:error.message
+        })
+    }
+}
---
+++ database/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+
+
+const DBConnection= async ()=>{
+    const MONGODB_URI=`mongodb+srv://file_sharingkey:sumitfile@file-sharing.phqykdg.mongodb.net/?retryWrites=true&w=majority`;
+    try {
+        await mongoose.connect(MONGODB_URI,{useNewUrlParser: true});
+        console.log("DB Connected Successfully");
+    } catch (error) {
+        console.error("Error while connecting the DataBase",error.message);
+    }
+}
+
+export default DBConnection;
