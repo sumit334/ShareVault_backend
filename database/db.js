46c815efca87f52f9777264719696e64d188fc30
Sumit Agarwal <97662774+sumit334@users.noreply.github.com>
2024-08-03 23:09:18 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 
 const DBConnection= async ()=>{
-    const MONGODB_URI=`mongodb+srv://file_sharingkey:sumitfile@file-sharing.phqykdg.mongodb.net/?retryWrites=true&w=majority`;
+    const MONGODB_URI=`mongodb+srv://file_sharingkey:sumitfile@file-sharing.phqykdg.mongodb.net/?retryWrites=true&w=majority&appName=file-sharing`;
     try {
         await mongoose.connect(MONGODB_URI,{useNewUrlParser: true});
         console.log("DB Connected Successfully");
